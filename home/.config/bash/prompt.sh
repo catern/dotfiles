7721865078cb709a50ec5bbe8de45073159412a0
catern <catern@catern.com>
2014-07-13 14:27:31 -0400

    add hostname-based hostname-coloring to prompt

--- home/.config/bash/prompt.sh
+++ home/.config/bash/prompt.sh
@@ -1,6 +1,7 @@
-# if root, don't include the username and color it red
+hostnamecolor=$(hostname | od | tr ' ' '\n' | awk '{total = total + $1}END{print 30 + (total % 7) + 1}')
+# if root, color username red
 	if [[ ${EUID} == 0 ]] ; then
-		PS1='\[\033[01;31m\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
+		PS1='\[\033[01;31m\]\u@\[\e[${hostnamecolor}m\]\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
 	else
-		PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
+		PS1='\[\033[01;32m\]\u@\[\e[${hostnamecolor}m\]\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
 	fi

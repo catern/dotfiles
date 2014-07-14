1791ad18601c155bf01f9865dcfda37ef7371fcf
catern <catern@catern.com>
2014-07-14 19:34:45 -0400

    fix root prompt too lOuLe

--- home/.config/bash/prompt.sh
+++ home/.config/bash/prompt.sh
@@ -1,7 +1,7 @@
 hostnamecolor=$(hostname | od | tr ' ' '\n' | awk '{total = total + $1}END{print 30 + (total % 7) + 1}')
 # if root, color username red
 	if [[ ${EUID} == 0 ]] ; then
-		PS1='\[\033[01;31m\]\u@\[\e[${hostnamecolor}m\]\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
+		PS1='\[\033[01;31m\]\u@\[\e[${hostnamecolor}m\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
 	else
 		PS1='\[\033[01;32m\]\u@\[\e[${hostnamecolor}m\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
 	fi

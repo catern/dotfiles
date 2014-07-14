82f80f5860a4a63a3c3e4edb2b28f2a1efe9c86f
sbaugh <sbaugh@janus>
2014-07-14 19:30:08 -0400

    remove garbage from prompt lol

--- home/.config/bash/prompt.sh
+++ home/.config/bash/prompt.sh
@@ -3,5 +3,5 @@ hostnamecolor=$(hostname | od | tr ' ' '\n' | awk '{total = total + $1}END{print
 	if [[ ${EUID} == 0 ]] ; then
 		PS1='\[\033[01;31m\]\u@\[\e[${hostnamecolor}m\]\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
 	else
-		PS1='\[\033[01;32m\]\u@\[\e[${hostnamecolor}m\]\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
+		PS1='\[\033[01;32m\]\u@\[\e[${hostnamecolor}m\]\h\[\033[01;34m\] \w \$\[\033[00m\] '
 	fi

259b5ad1e759ae83c363d7acdd24889540794d85
catern <catern@catern.com>
2015-01-12 12:46:07 -0500

    bash: don't override TERM when COLORTERM is set

--- home/.config/bash/vte.sh
+++ home/.config/bash/vte.sh
@@ -2,7 +2,7 @@
 # applications avoided using 256colors. Detect VTE and set an
 # appropriate TERM value.
 # This is fixed upstream now, but not yet packaged.
-if [ -n "$VTE_VERSION" ] || [ -n "$COLORTERM" ]
+if [ -n "$VTE_VERSION" ]
 then
     TERM=xterm-256color
 fi

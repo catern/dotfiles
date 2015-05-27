66d8b70411bbd27f16e784b081ddb1aa6b2c070c
catern <catern@catern.com>
2015-05-27 17:09:00 -0400

    club: add clubrootkl alias

--- home/.config/bash/aliases.sh
+++ home/.config/bash/aliases.sh
@@ -1,5 +1,6 @@
-alias clubkl='kinit -l 10d $USER@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
-alias clubadminkl='kinit -l 10d $USER/admin@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
+alias clubkl='kinit -f -l 10d $USER@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
+alias clubrootkl='kinit -f -l 10d $USER/root@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
+alias clubadminkl='kinit -f -l 10d $USER/admin@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
 alias kl='kinit -l 10d $USER@ANDREW.CMU.EDU && aklog andrew.cmu.edu'
 
 alias g='git'

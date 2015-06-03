dde00b4554ed316bb9981af7bc9ff20bc30bcb0f
catern <catern@catern.com>
2015-06-03 14:05:27 -0400

    vars: let ~/.local/ override /usr/

--- home/.config/bash/vars.sh
+++ home/.config/bash/vars.sh
@@ -1,5 +1,7 @@
 # put go bin/ pkg/ src/ in .local
 export GOPATH=$HOME/.local
 # make any executables in .local/bin available for command-line use
-export PATH=$PATH:~/.local/bin
+# and override system executables
+export PATH=~/.local/bin:$PATH
+export MANPATH=~/.local/share/man:$MANPATH
 export EDITOR=emx

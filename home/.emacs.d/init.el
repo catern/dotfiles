dfe797f12df4b3a27a9dcbe5bcbce03a708faf68
catern <catern@catern.com>
2015-06-30 20:26:20 -0400

    emacs: remove remapping of Escape to keyboard-quit
    I've never used these Escape remappings anyway, and they occassionally
    annoy me. Plus they clutter up my init.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -101,26 +101,6 @@
 ; be a little more lenient to tolerate key delay over ssh
 (setq evil-esc-delay .05)
 
-; usable esc
-(defun minibuffer-keyboard-quit () 
-  "Abort recursive edit.
-  In Delete Selection mode, if the mark is active, just deactivate it;
-  then it takes a second \\[keyboard-quit] to abort the minibuffer."
-  (interactive)
-  (if (and delete-selection-mode transient-mark-mode mark-active)
-    (setq deactivate-mark  t)
-    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))(abort-recursive-edit)))
-
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-(define-key isearch-mode-map [escape] 'isearch-cancel)
-(global-set-key [escape] 'keyboard-escape-quit)
-
 ;;;; clojure
 (evil-define-key 'normal cider-mode-map 
   "q" 'cider-popup-buffer-quit-function

b29f4ab829ff6dc8c2b8b6b07edd3cf7e43120ba
Spencer Baugh <Spencer.baugh@purestorage.com>
2015-08-14 00:59:49 -0700

    bash: increase in-memory history size to 5MB

--- home/.config/bash/history.sh
+++ home/.config/bash/history.sh
@@ -1,8 +1,8 @@
 # This file brought to you by http://mywiki.wooledge.org/BashFAQ/088
 # have an extremely large on-disk history of 400MB
 HISTFILESIZE=$((400 * 1000 * 1000))
-# have a large in-memory history of 1MB
-HISTSIZE=$((1000 * 1000))
+# have a large in-memory history of 5MB
+HISTSIZE=$((5 * 1000 * 1000))
 # append to the history file, don't overwrite it
 shopt -s histappend
 # don't ignore any commands in my history

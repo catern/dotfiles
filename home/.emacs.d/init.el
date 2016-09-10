f51cbdd73b094147c31187110c2b4d7fa34ce942
catern <catern@catern.com>
2016-09-09 20:45:23 -0400

    emacs: evil: default to emacs-state
    We default to emacs-state, and determine programmatically what modes are
    text-editing heavy and use normal-state in them.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -78,6 +78,53 @@
 (setq undo-tree-auto-save-history t
       undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree-persist")))
 (require 'evil)
+
+;; Default to emacs-state and only use normal-state in modes where
+;; we'll be doing a lot of text editing.
+(setq evil-default-state 'emacs
+      evil-emacs-state-modes nil
+      evil-insert-state-modes nil
+      evil-motion-state-modes nil
+      evil-normal-state-modes '(text-mode prog-mode fundamental-mode
+					  css-mode conf-mode
+					  TeX-mode LaTeX-mode
+					  org-mode
+					  diff-mode))
+
+;; Furthermore, we'll override evil-initial state to also put us in
+;; normal-state in modes derived from those text editing modes, which
+;; should cover just about everything we want it to cover.
+;; First we need some helpers:
+(defun my-real-function (fun)
+  "Figure out the actual symbol behind a function.
+Returns a different symbol if FUN is an alias, otherwise FUN."
+  (let ((symbol-function (symbol-function fun)))
+    (if (symbolp symbol-function)
+	symbol-function
+      fun)))
+
+(defun my-derived-mode-p (mode modes)
+  "Non-nil if the current major mode is derived from one of MODES."
+  (let ((parent (my-real-function mode)))
+    (while (and parent (not (memq parent modes)))
+      (setq parent (my-real-function (get parent 'derived-mode-parent))))
+    parent))
+
+;; Override here:
+(defun evil-initial-state (mode &optional default)
+  "Return the Evil state to use for MODE.
+Returns DEFAULT if no initial state is associated with MODE.
+The initial state for a mode can be set with
+`evil-set-initial-state'."
+  (let (state modes)
+    (catch 'done
+      (dolist (entry (nreverse (evil-state-property t :modes)) default)
+	(setq state (car entry)
+	      modes (symbol-value (cdr entry)))
+	(when (or (memq mode modes)
+		  (my-derived-mode-p mode modes))
+	  (throw 'done state)))))))
+
 (evil-mode 1)
 
 ;; unmap everything but Esc from evil-insert-state-map

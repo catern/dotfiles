0dabe7f08165c03b7d96c182ce9f09494cf74740
catern <catern@catern.com>
2016-09-24 12:26:34 -0400

    emacs: lifelog: move into separate file

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -84,40 +84,11 @@
 ; do syntax highlighting in #+begin_src blocks
 (setq org-src-fontify-natively t)
 
-;;;; life-logging
-(setq my-buffer-activity-logfile (expand-file-name "~/.emacs.d/bufferlog"))
-
-(defun my-store-name-of-active-buffer ()
-  "Stores the name of the active buffer in my-buffer-activity-logfile"
-  (interactive)
-  (write-region 
-   (concat (format-time-string "%Y-%m-%d %H:%M:%S") 
-           " "
-           (or (buffer-file-name (current-buffer))
-               (buffer-name (current-buffer))
-               )
-           "\n") 
-   nil my-buffer-activity-logfile 'append 'silent)
-  )
-  
-(defun my-store-lifelog-data ()
-  "Activates all my lifelogging functions"
-  (interactive)
-  (if 
-      (time-less-p 
-       (or 
-        (current-idle-time) 
-        (seconds-to-time 0))
-       (seconds-to-time 5) 
-       )
-  (my-store-name-of-active-buffer))
-)
-
-(setq lifelog-timer (when (daemonp)
-		      (run-with-timer 1 5 'my-store-lifelog-data)))
-
 ;;;; dired
 (setq dired-dwim-target t)
+
+;;;; lifelog
+(load-file "~/.emacs.d/lifelog.el")
 
 ;; local.el
 (setq my-local-config-file "~/.emacs.d/local.el")

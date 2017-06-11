aacf41b5e57eea3fc78e4794a2365c6d06acb31e
Spencer Baugh <sbaugh@catern.com>
2017-06-11 14:03:01 +0000

    bash: delete unused aliases

--- home/.config/bash/aliases.sh
+++ home/.config/bash/aliases.sh
@@ -1,18 +1,3 @@
-alias clubkl='kinit -f -l 10d $USER@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
-alias clubrootkl='kinit -f -l 10d $USER/root@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
-alias clubadminkl='kinit -f -l 10d $USER/admin@CLUB.CC.CMU.EDU && aklog club.cc.cmu.edu'
-alias kl='kinit -l 10d $USER@ANDREW.CMU.EDU && aklog andrew.cmu.edu'
-
 alias g='git'
-alias e='emx'
-alias vi='emx'
-
 alias atch='tmux attach -d'
-alias dup='nohup urxvt >> ~/.urxvt/nohup.out &'
-
-alias ..='cd ..'
-alias ....='cd ../..'
-alias ......='cd ../../..'
-alias ........='cd ../../../..'
-
 alias topdf='soffice --headless --convert-to pdf'

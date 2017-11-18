5281619ff21f46ec7ba39a9132065a6e8d3e6479
catern <catern@catern.com>
2017-11-18 15:01:07 -0500

    nix: add Emacs package configuration to config.nix

--- home/.config/nixpkgs/config.nix
+++ home/.config/nixpkgs/config.nix
@@ -1,4 +1,27 @@
 {
   allowUnfree = true;
   allowBroken = true;
+  packageOverrides = pkgs: with pkgs; rec {
+    myEmacs = emacsWithPackages (epkgs: (with epkgs; [
+      better-defaults
+      undo-tree
+      cyberpunk-theme
+      magit
+      circe
+      csv-mode
+      yaml-mode
+      toml-mode
+      rust-mode
+      nix-mode
+      geiser
+      restclient
+      ess
+      intero
+      nyan-mode
+      slime-volleyball
+      ix
+      tuareg
+      merlin
+    ]));
+  };
 }
